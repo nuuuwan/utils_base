6c2ef3e194bcf38cd34689618ddbef725e710872
nuuuwan <nuuuwan@gmail.com>
2025-10-02 15:05:13 +0530

    Added Format

--- src/utils_base/__init__.py
+++ src/utils_base/__init__.py
@@ -3,7 +3,7 @@
 
 from utils_base.console import (COLOR_BACKGROUND, COLOR_FOREGROUND,
                                 COLOR_FORMAT, LEVEL_TO_STYLE, Console, Log)
-from utils_base.ds import Chunker, Dict, Iter, List, Parse, String
+from utils_base.ds import Chunker, Dict, Format, Iter, List, Parse, String
 from utils_base.file import (BigJSONFile, CSVFile, File, FileOrDirectory,
                              JSONFile, Markdown, PDFFile, PDFFileCompressMixin,
                              PDFTextMixin, TSVFile, XSVFile)
--- src/utils_base/ds/__init__.py
+++ src/utils_base/ds/__init__.py
@@ -3,6 +3,7 @@
 
 from utils_base.ds.Chunker import Chunker
 from utils_base.ds.Dict import Dict
+from utils_base.ds.Format import Format
 from utils_base.ds.Iter import Iter
 from utils_base.ds.List import List
 from utils_base.ds.Parse import Parse
